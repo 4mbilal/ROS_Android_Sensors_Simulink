82f2afa9944cf4e085774354de86016516cf33bf
4mbilal <4mbilal@gmail.com>
2020-04-29 19:34:40 +0300

    clean up

--- downloaded_examples/IMUControl/src/main/java/com/example/IMUControl/OnFragmentInteractionListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.IMUControl;
-
-
-interface OnFragmentInteractionListener {
-   void onFragmentCreate (String name);
-   void onFragmentStart  (String name);
-   void onFragmentPause  (String name);
-   void onFragmentResume (String name);
-}
